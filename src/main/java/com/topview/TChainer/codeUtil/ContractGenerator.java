9f1631df7907f74b4ed8808f48909af0d765c444
liu <a2506991786@qq.com>
2023-09-21 00:24:53 +0800

    feat:finishing-contractGenerator

--- src/main/java/com/topview/TChainer/codeUtil/ContractGenerator.java
+++ src/main/java/com/topview/TChainer/codeUtil/ContractGenerator.java
@@ -3,8 +3,12 @@ package com.topview.TChainer.codeUtil;
 import com.sun.javafx.util.Logging;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @Slf4j
 public class ContractGenerator {
@@ -28,44 +32,57 @@ public class ContractGenerator {
     }
 
 
-    public static String generateContract(Class<?> beanClass) {
+    public static void generateContract(Class<?> beanClass) {
         StringBuilder sb = new StringBuilder();
         String contractName = beanClass.getSimpleName();
         sb.append("// SPDX-License-Identifier: MIT\n");
         sb.append("pragma solidity >=0.6.10 <0.8.11;\n\n");
         sb.append("import \"./DataContract.sol\";\n\n");
         sb.append("contract ").append(contractName).append(" is DataContract {\n");
+        sb.append("uint256 private latestDataIndex = 0;\n\n");
         sb.append("    struct Data {\n");
         StringBuilder decodeParams = new StringBuilder();
         StringBuilder decodeReceiveParams = new StringBuilder();
+        StringBuilder inputParams = new StringBuilder();
         try {
             int length = beanClass.getDeclaredFields().length;
             for (Field field : beanClass.getDeclaredFields()) {
                 length--;
                 String javaType = field.getType().getSimpleName();
                 String solidityType = typeMapping.getOrDefault(javaType, "unknown");
-                if (length>0) {
-                    decodeReceiveParams.append(solidityType).append(" ").append(field.getName()).append(", ");
-                    decodeParams.append(solidityType).append(", ");
-                }else {
-                    decodeReceiveParams.append(solidityType).append(" ").append(field.getName());
-                    decodeParams.append(solidityType);
+                if (field.getName() != "id") {
+                    if (length > 0) {
+                        decodeReceiveParams.append(solidityType).append(" ").append(field.getName()).append(", ");
+                        decodeParams.append(solidityType).append(", ");
+                    } else {
+                        decodeReceiveParams.append(solidityType).append(" ").append(field.getName());
+                        decodeParams.append(solidityType);
+                    }
+
+                    inputParams.append("        inputData.").append(field.getName()).append("=").append(field.getName()).append(";\n");
                 }
-
                 String fieldName = field.getName();
-
                 sb.append("        ").append(solidityType).append(" ").append(fieldName).append(";\n");
             }
+
             sb.append("    }\n\n");
 
+            sb.append("    mapping(uint256 => Data) private dataMap;\n\n");
+            sb.append("    mapping(uint256 => uint256) private blockHeightMap;\n\n");
+            sb.append("    // @param place 用于解决同一区块中有相同事件问题\n");
+            sb.append("    event execute").append(beanClass.getSimpleName()).append("Event(uint256 blockHeight,uint8 place, Data data);\n\n");
+
             // 添加自定义的函数
-            sb.append("\n    function add(bytes memory data) public override returns (uint256) {\n");
+            sb.append("\n    function add(bytes memory data) public override returns (bool) {\n");
             sb.append("        Data memory inputData;\n");
             sb.append("        (").append(decodeReceiveParams).append(") = abi.decode(data, (").append(decodeParams).append("));\n");
-            sb.append("        self.id++;\n");
-            sb.append("        // 解包Data结构体数据并进行处理\n");
-            sb.append("        // TODO: 根据需要进行具体的处理\n");
-            sb.append("        return self.id;\n");
+            sb.append("        inputData.id = latestDataIndex;\n");
+            sb.append(inputParams);
+            sb.append("        latestDataIndex++;\n");
+            sb.append("        dataMap[latestDataIndex] = inputData;\n");
+            sb.append("        emit  executeEvent(block.number, inputData);\n");
+            sb.append("        blockHeightMap[latestDataIndex] = block.number;\n");
+            sb.append("        return true;\n");
             sb.append("    }\n");
 
 
@@ -74,7 +91,20 @@ public class ContractGenerator {
         }
 
         sb.append("}\n");
+        String contractCode = sb.toString();
+        String filePath = "src/main/resources/"+beanClass.getSimpleName()+".sol";
+        writeContractToFile(contractCode, filePath);
 
-        return sb.toString();
+    }
+
+    public static void writeContractToFile(String contractCode, String filePath) {
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(contractCode);
+            writer.close();
+            System.out.println("Contract code written to file: " + filePath);
+        } catch (IOException e) {
+            log.error("write contract to file error", e);
+        }
     }
 }
